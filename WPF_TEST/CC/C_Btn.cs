807c6b939c6f8feefebae55adac525ac6a7559e2
mozi92@icoud.com <yajun320>
2021-05-12 14:07:00 +0800

    初始化项目

---
+++ WPF_TEST/CC/C_Btn.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_TEST.CC
+{
+    /// <summary>
+    /// 按照步骤 1a 或 1b 操作，然后执行步骤 2 以在 XAML 文件中使用此自定义控件。
+    ///
+    /// 步骤 1a) 在当前项目中存在的 XAML 文件中使用该自定义控件。
+    /// 将此 XmlNamespace 特性添加到要使用该特性的标记文件的根
+    /// 元素中:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:WPF_TEST.CC"
+    ///
+    ///
+    /// 步骤 1b) 在其他项目中存在的 XAML 文件中使用该自定义控件。
+    /// 将此 XmlNamespace 特性添加到要使用该特性的标记文件的根
+    /// 元素中:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:WPF_TEST.CC;assembly=WPF_TEST.CC"
+    ///
+    /// 您还需要添加一个从 XAML 文件所在的项目到此项目的项目引用，
+    /// 并重新生成以避免编译错误:
+    ///
+    ///     在解决方案资源管理器中右击目标项目，然后依次单击
+    ///     “添加引用”->“项目”->[浏览查找并选择此项目]
+    ///
+    ///
+    /// 步骤 2)
+    /// 继续操作并在 XAML 文件中使用控件。
+    ///
+    ///     <MyNamespace:C_Btn/>
+    ///
+    /// </summary>
+    public partial class C_Btn : Button
+    {
+        public static readonly DependencyProperty PressedBackgroundProperty =
+            DependencyProperty.Register("PressedBackground", typeof(Brush), typeof(C_Btn), new PropertyMetadata(Brushes.DarkBlue));
+        /// <summary>
+        /// 鼠标按下背景样式
+        /// </summary>
+        public Brush PressedBackground
+        {
+            get { return (Brush)GetValue(PressedBackgroundProperty); }
+            set { SetValue(PressedBackgroundProperty, value); }
+        }
+
+        public static readonly DependencyProperty PressedForegroundProperty =
+            DependencyProperty.Register("PressedForeground", typeof(Brush), typeof(C_Btn), new PropertyMetadata(Brushes.White));
+        /// <summary>
+        /// 鼠标按下前景样式（图标、文字）
+        /// </summary>
+        public Brush PressedForeground
+        {
+            get { return (Brush)GetValue(PressedForegroundProperty); }
+            set { SetValue(PressedForegroundProperty, value); }
+        }
+
+        public static readonly DependencyProperty MouseOverBackgroundProperty =
+            DependencyProperty.Register("MouseOverBackground", typeof(Brush), typeof(C_Btn), new PropertyMetadata(Brushes.RoyalBlue));
+        /// <summary>
+        /// 鼠标进入背景样式
+        /// </summary>
+        public Brush MouseOverBackground
+        {
+            get { return (Brush)GetValue(MouseOverBackgroundProperty); }
+            set { SetValue(MouseOverBackgroundProperty, value); }
+        }
+
+        public static readonly DependencyProperty MouseOverForegroundProperty =
+            DependencyProperty.Register("MouseOverForeground", typeof(Brush), typeof(C_Btn), new PropertyMetadata(Brushes.White));
+        /// <summary>
+        /// 鼠标进入前景样式
+        /// </summary>
+        public Brush MouseOverForeground
+        {
+            get { return (Brush)GetValue(MouseOverForegroundProperty); }
+            set { SetValue(MouseOverForegroundProperty, value); }
+        }
+
+        public static readonly DependencyProperty FIconProperty =
+            DependencyProperty.Register("FIcon", typeof(string), typeof(C_Btn), new PropertyMetadata("\ue604"));
+        /// <summary>
+        /// 按钮字体图标编码
+        /// </summary>
+        public string FIcon
+        {
+            get { return (string)GetValue(FIconProperty); }
+            set { SetValue(FIconProperty, value); }
+        }
+
+        public static readonly DependencyProperty FIconSizeProperty =
+            DependencyProperty.Register("FIconSize", typeof(int), typeof(C_Btn), new PropertyMetadata(20));
+        /// <summary>
+        /// 按钮字体图标大小
+        /// </summary>
+        public int FIconSize
+        {
+            get { return (int)GetValue(FIconSizeProperty); }
+            set { SetValue(FIconSizeProperty, value); }
+        }
+
+        public static readonly DependencyProperty FIconMarginProperty = DependencyProperty.Register(
+            "FIconMargin", typeof(Thickness), typeof(C_Btn), new PropertyMetadata(new Thickness(0, 1, 3, 1)));
+        /// <summary>
+        /// 字体图标间距
+        /// </summary>
+        public Thickness FIconMargin
+        {
+            get { return (Thickness)GetValue(FIconMarginProperty); }
+            set { SetValue(FIconMarginProperty, value); }
+        }
+
+        public static readonly DependencyProperty AllowsAnimationProperty = DependencyProperty.Register(
+            "AllowsAnimation", typeof(bool), typeof(C_Btn), new PropertyMetadata(true));
+        /// <summary>
+        /// 是否启用Ficon动画
+        /// </summary>
+        public bool AllowsAnimation
+        {
+            get { return (bool)GetValue(AllowsAnimationProperty); }
+            set { SetValue(AllowsAnimationProperty, value); }
+        }
+
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.Register("CornerRadius", typeof(CornerRadius), typeof(C_Btn), new PropertyMetadata(new CornerRadius(2)));
+        /// <summary>
+        /// 按钮圆角大小,左上，右上，右下，左下
+        /// </summary>
+        public CornerRadius CornerRadius
+        {
+            get { return (CornerRadius)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+
+        public static readonly DependencyProperty ContentDecorationsProperty = DependencyProperty.Register(
+            "ContentDecorations", typeof(TextDecorationCollection), typeof(C_Btn), new PropertyMetadata(null));
+        public TextDecorationCollection ContentDecorations
+        {
+            get { return (TextDecorationCollection)GetValue(ContentDecorationsProperty); }
+            set { SetValue(ContentDecorationsProperty, value); }
+        }
+        static C_Btn()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(C_Btn), new FrameworkPropertyMetadata(typeof(C_Btn)));
+        }
+
+
+        /// <summary>
+        /// 注册导航名称
+        /// </summary>
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register("Value", typeof(string), typeof(C_Btn), new FrameworkPropertyMetadata("文本"));
+        public string Value
+        {
+            get { return (string)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+
+        /// <summary>
+        /// 注册图片路径属性
+        /// </summary>
+        public static readonly DependencyProperty SrcProperty = DependencyProperty.Register("Src", typeof(string), typeof(C_Btn), new FrameworkPropertyMetadata(GetPath("/Resource/库存调整.png")));
+        public string Src
+        {
+            get { return (string)GetValue(SrcProperty); }
+            set { SetValue(SrcProperty, value); }
+        }
+
+        static string GetPath(string uri)
+        {
+            string path = Directory.GetCurrentDirectory();
+            FileInfo directory = new FileInfo(path);
+            FileInfo directory2 = new FileInfo(directory.DirectoryName);
+            return directory2.DirectoryName + uri;
+        }
+    }
+}
---
+++ WPF_TEST/UC/U_Nav.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_TEST.UC
+{
+    /// <summary>
+    /// U_Nav.xaml 的交互逻辑
+    /// </summary>
+    public partial class U_Nav : UserControl
+    {
+        public U_Nav()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 添加文本属性
+        /// </summary>
+        public static readonly DependencyProperty TextProperty =
+                   DependencyProperty.Register("Text", typeof(string),
+                   typeof(U_Nav),
+                   new PropertyMetadata("TextBlock", new PropertyChangedCallback(OnTextChanged)));
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+
+            set { SetValue(TextProperty, value); }
+        }
+
+        static void OnTextChanged(object sender, DependencyPropertyChangedEventArgs args)
+        {
+            U_Nav source = (U_Nav)sender;
+            source.tb.Text = (string)args.NewValue;
+        }
+
+        /// <summary>
+        /// 注册图片路径属性
+        /// </summary>
+        public static readonly DependencyProperty SrcProperty = DependencyProperty.Register("Src", typeof(string), typeof(U_Nav), new PropertyMetadata("Image", new PropertyChangedCallback(OnSrcChanged)));
+        public string Src
+        {
+            get { return (string)GetValue(SrcProperty); }
+            set { SetValue(SrcProperty, value); }
+        }
+
+        static void OnSrcChanged(object sender, DependencyPropertyChangedEventArgs args)
+        {
+            U_Nav source = (U_Nav)sender;
+            if (args.NewValue != null)
+                source.icon.Source = GetSource((string)args.NewValue);
+        }
+
+        static BitmapImage GetSource(string uri)
+        {
+            string path = Directory.GetCurrentDirectory();
+            FileInfo directory = new FileInfo(path);
+            FileInfo directory2 = new FileInfo(directory.DirectoryName);
+            string sss = directory2.DirectoryName + uri;
+            return new BitmapImage(new Uri(sss, UriKind.Absolute));
+        }
+
+
+
+        public static readonly RoutedEvent MyButtonClickEvent =
+            EventManager.RegisterRoutedEvent("MyButtonClick", RoutingStrategy.Bubble, typeof(RoutedPropertyChangedEventHandler<object>), typeof(U_Nav));
+
+        public event RoutedPropertyChangedEventHandler<object> MyButtonClick
+        {
+            add
+            {
+                this.AddHandler(MyButtonClickEvent, value);
+            }
+
+            remove
+            {
+                this.RemoveHandler(MyButtonClickEvent, value);
+            }
+        }
+
+        public void OnMyButtonClick(object oldValue, object newValue)
+        {
+            RoutedPropertyChangedEventArgs<object> arg =
+                new RoutedPropertyChangedEventArgs<object>(oldValue, newValue, MyButtonClickEvent);
+
+            this.RaiseEvent(arg);
+        }
+    }
+}
